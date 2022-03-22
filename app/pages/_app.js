e1a792cf3829d889e5093ab976edac9c88f9fe81
Julien ANQUETIL <anquetil.jul@gmail.com>
2022-03-22 16:24:32 +0100

    event observer + start button + animation

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -2,6 +2,7 @@ import AuthProvider from "../context/AuthProvider";
 import useAuth from "../hooks/useAuth";
 
 import '../styles/globals.css'
+import '../styles/animate.min.css'
 const MyApp = ({ Component, pageProps }) => {
 
 	const {user} = useAuth()
---
+++ app/pages/game.js
@@ -0,0 +1,40 @@
+import { useEffect, useImperativeHandle, useState } from 'react';
+import UserInterface from '../objects/UserInterface';
+import Observer, { EVENTS } from '../Observer';
+
+import styles from '../styles/Home.module.css'
+export default function Home() {
+    const [userInterface, setUserInterface] = useState(null);
+
+    useEffect(() => {
+        if (!userInterface) {
+            let inter = new UserInterface();
+            setUserInterface(inter);
+        }
+    }, [])
+
+    useEffect(() => {
+        if (userInterface) {
+            events();
+        }
+    }, [userInterface])
+
+    const events = () => {
+        Observer.emit(EVENTS.NEW_GAME);
+    }
+
+	return (
+		<div className={styles.container}>
+            <div id="points" className="animate__animated noselect">0</div>
+            <div id="button-start" className="animate__animated noselect">
+                <div>StackBlocks</div>
+                <button>Start</button>
+            </div>
+            <div id="gameover" className="animate__animated noselect">
+                Game Over
+            </div>
+
+            <div id="game-container"></div>
+		</div>
+	)
+}
