9504e9937eee4bdb294e966a9563097aac3d8008
Felipe Alarcon <zonbimutin@gmail.com>
2022-04-12 08:13:05 +0200

    Add AuthModal

--- app/components/Global/Header.js
+++ app/components/Global/Header.js
@@ -1,24 +1,24 @@
+import Image from "next/image";
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import AuthModal from "../Modals/AuthModal";
+import logo from "../../public/logo.png"
 
 const Header = () => {
+	
 	return (
 		<Navbar bg="light" expand="lg">
 			<Container>
-				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<Navbar.Brand href="#home">
+					<Image src={'/logo.png'} width={40} height={40}/>
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#link">Link</Nav.Link>
-						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-						</NavDropdown>
+						<Nav.Link href="/game">Start Game</Nav.Link>
+						<Nav.Link href="/scores">Scores</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
+				<AuthModal/>
 			</Container>
 		</Navbar>
 	)
